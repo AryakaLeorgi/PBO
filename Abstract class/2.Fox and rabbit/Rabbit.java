169a0479e7f7d0c400b9c5ee2df1f1d9d1fcd4b8
AryakaLeorgi <155879430+AryakaLeorgi@users.noreply.github.com>
2024-12-04 18:51:58 +0700

    Update Rabbit.java

--- Abstract class/2.Fox and rabbit/Rabbit.java
+++ Abstract class/2.Fox and rabbit/Rabbit.java
@@ -1,18 +1,22 @@
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Rabbit extends Animal {
-    public Rabbit(Field field, Location location) {
-        super(field, location);
+    public Rabbit(Field field, int row, int col) {
+        super(field, row, col);
     }
+
     @Override
-    public void act(List<Animal> newAnimals){
-        if(isAlive()){
-            Location newLocation = getField().freeAdjacentLocation(getLocation());
-            if(newLocation != null){
-                setLocation(newLocation);
-            }
-            else {
-                setDead();
+    public void act() {
+        ArrayList<int[]> adjacent = field.getAdjacent(row, col);
+        Random rand = new Random();
+        if (!adjacent.isEmpty()) {
+            int[] newLocation = adjacent.get(rand.nextInt(adjacent.size()));
+            int newRow = newLocation[0];
+            int newCol = newLocation[1];
+            if (field.getObjectAt(newRow, newCol) == null) {
+                System.out.println("Rabbit at (" + row + ", " + col + ") moved to (" + newRow + ", " + newCol + ").");
+                move(newRow, newCol);
             }
         }
     }
